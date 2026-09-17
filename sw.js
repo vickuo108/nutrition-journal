550a71c92c553da51849e3f795bedc2d74946e07
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 01:39:44 +0800

    Keep weekly summary dates on one line

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-4";
+const CACHE = "nutrition-2026-09-18-5";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
