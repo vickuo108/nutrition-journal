3dcb3012a71f4411920b39cd6b5d0a90d4102fd2
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 01:56:03 +0800

    Improve behavior editing and meal modal close

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-6";
+const CACHE = "nutrition-2026-09-18-7";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
