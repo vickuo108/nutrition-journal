a468ac18ec7c8180d57d48389411688ed106fc51
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 21:52:16 +0800

    Add food exchange hints

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-25";
+const CACHE = "nutrition-2026-09-18-26";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
