c3191da54523864c531c884496965a869cba1b7c
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 14:16:13 +0800

    Shorten review chart labels

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-10";
+const CACHE = "nutrition-2026-09-18-11";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
