7a7a933e62f83d5e56043f83393efde5159e80cc
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 22:05:03 +0800

    Refine exchange hints and calorie chart axis

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-29";
+const CACHE = "nutrition-2026-09-18-30";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
