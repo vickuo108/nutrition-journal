15c3bbcd5f5cd3b9f588ccc0d3702f476a9fceae
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 21:43:58 +0800

    Change review food average to calories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-24";
+const CACHE = "nutrition-2026-09-18-25";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
