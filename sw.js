7e30617b747f469af0f0736c3b20e13a2718b1cc
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 22:13:02 +0800

    Add sticky review navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-30";
+const CACHE = "nutrition-2026-09-18-31";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
