708292d9e4cf66a3dfe535e22236af017c9b9759
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 21:42:04 +0800

    Show daily calories in history

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-23";
+const CACHE = "nutrition-2026-09-18-24";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
