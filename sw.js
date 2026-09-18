574d8cc63b2c4b143390a204711a1954e9e25967
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 22:19:06 +0800

    Refine sticky review controls layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-31";
+const CACHE = "nutrition-2026-09-18-32";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
