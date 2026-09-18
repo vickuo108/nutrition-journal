ba5bee0661eda8031ca3c0d93d630859aa814406
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-18 14:49:33 +0800

    Add behavior shortcut and align day type

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-18-12";
+const CACHE = "nutrition-2026-09-18-13";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
