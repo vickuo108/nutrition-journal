b6f957dc9235e060f62510c0bb307a5c53c261d5
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 19:38:01 +0800

    Reorder meals when meal number changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-44";
+const CACHE = "nutrition-2026-09-19-45";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
