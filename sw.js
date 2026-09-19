6c710eca58f530fe8b49592e6991700fc712c1c2
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 21:07:36 +0800

    Format review averages with separators

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-55";
+const CACHE = "nutrition-2026-09-19-56";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
