aee53093dcf0a8b345b88555b6548ba5d550696d
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 21:24:11 +0800

    Make monthly summary tables horizontally scrollable

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-58";
+const CACHE = "nutrition-2026-09-19-59";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
