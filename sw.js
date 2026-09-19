ff7fa86e5d455d6cae05ebe61de916855d44ca92
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 21:13:18 +0800

    Preview summary image for mobile save

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-57";
+const CACHE = "nutrition-2026-09-19-58";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
