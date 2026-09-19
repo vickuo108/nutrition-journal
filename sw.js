c86113af1e1a9480614316017f11ff7c4e659f86
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 19:59:07 +0800

    Use bar chart for sleep review

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-50";
+const CACHE = "nutrition-2026-09-19-51";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
