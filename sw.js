35661ce1cbd6756dacdb605af4bc2430c3a99291
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 19:46:34 +0800

    Place food exchange hint over label

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-48";
+const CACHE = "nutrition-2026-09-19-49";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
