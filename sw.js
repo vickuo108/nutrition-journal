c628dd3bb22e0d82ce124af66cfa71b25c2583cb
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 19:19:29 +0800

    Copy history meal to today

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-42";
+const CACHE = "nutrition-2026-09-19-43";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
