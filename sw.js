08c34e709ecfa61c769e673c502f11c6fa5d954a
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 19:15:39 +0800

    Move meal copy action to edit modal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-41";
+const CACHE = "nutrition-2026-09-19-42";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
