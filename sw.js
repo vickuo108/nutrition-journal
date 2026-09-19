2e9901bf322b03231858b53b107fe918fdb47bb4
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 21:11:03 +0800

    Fix summary image download

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-56";
+const CACHE = "nutrition-2026-09-19-57";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
