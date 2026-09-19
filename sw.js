efaac3827c3b5c430ec8e5dabd18202b2f736502
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-19 20:10:16 +0800

    Show steps in review date cards

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-19-51";
+const CACHE = "nutrition-2026-09-19-52";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
