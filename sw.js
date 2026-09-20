f634c54c4da57d9db68a262266db898c6aac90bf
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-20 19:52:09 +0800

    Keep keyboard open while editing food

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-20-1";
+const CACHE = "nutrition-2026-09-20-2";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
