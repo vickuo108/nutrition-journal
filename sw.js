2735743a9d18ae4ab5faafae8e1e8cc17251d64c
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-20 20:29:33 +0800

    Keep meal form keyboard open while selecting categories

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-20-4";
+const CACHE = "nutrition-2026-09-20-5";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
