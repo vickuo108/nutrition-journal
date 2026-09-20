ba2f79ca7d9d4a318ede9038e778d6e79ca5f4e2
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-20 19:56:50 +0800

    Aggregate meal portion summaries

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-20-3";
+const CACHE = "nutrition-2026-09-20-4";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
