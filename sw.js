60aa1cc9c7c6248e667b2fd6d142ec25c1190762
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-21 02:35:49 +0800

    Fix cumulative app version numbering

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-21-1";
+const CACHE = "nutrition-2026-09-21-66";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
