da85a3dced2dd01adca869958cae775ee1127e6e
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-21 13:44:14 +0800

    Keep meal inputs visible above keyboard

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-21-67";
+const CACHE = "nutrition-2026-09-21-68";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
