89858df87cfc52cc4bc30c2aae2fc8d2463afdf4
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-21 13:52:26 +0800

    Tune mobile meal form keyboard scrolling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-21-68";
+const CACHE = "nutrition-2026-09-21-69";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
