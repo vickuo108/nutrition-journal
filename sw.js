39abc295b3c8cf1d7abd3c491703383835745865
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-21 14:41:55 +0800

    Add mobile keyboard spacer for meal form

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-21-71";
+const CACHE = "nutrition-2026-09-21-72";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
