3d8c703db080eb553e3cea45d0fbf63a23797e3f
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-22 13:15:43 +0800

    Restore native meal form keyboard scrolling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-21-72";
+const CACHE = "nutrition-2026-09-22-73";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
