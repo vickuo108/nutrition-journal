5ea6f2f220502e0e657d53dd8435af7eae769d18
郭羽軒 <kuoyuhsuan@guoyuxuandeiMac.local>
2026-09-24 00:50:28 +0800

    Add body metrics review section

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nutrition-2026-09-23-75";
+const CACHE = "nutrition-2026-09-24-76";
 const APP_SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icons/app-icon.svg"];
 
 self.addEventListener("install", event => {
